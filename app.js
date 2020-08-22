7ea7602aae08c516dfd5608e1fa0a393ec3eb40d
shafi10 <shafiswe@gmail.com>
2020-08-22 14:43:10 +0600

    seven commit

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express') 
+const path = require('path');
 
 const app = express()
 
 const newsRouter = require('./routes/news')
 app.use('/news', newsRouter);
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+    }
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
